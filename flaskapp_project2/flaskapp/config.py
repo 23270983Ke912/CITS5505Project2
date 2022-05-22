6eae9adde6e30d27d5dca6d7a5409ccd0bae8414
Xinyu Wang <wanxy124@Xinyus-MBP.modem>
2022-05-22 23:37:27 +0800

    minor fix

--- flaskapp_project2/flaskapp/config.py
+++ flaskapp_project2/flaskapp/config.py
@@ -1,2 +1,22 @@
-CSRF_ENABLED = True
-SECRET_KEY = 'you-can-never-guess-lol'
+import os
+import flaskapp
+from dotenv import load_dotenv
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '233333!'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'app.db')
+    DATABASE=os.path.join(flaskapp.app.instance_path, 'flaskapp.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    ENV='production'
+    SECRET_KEY = os.environ['SECRET_KEY']
+    DATABASE=os.path.join(flaskapp.app.instance_path, 'flaskapp.sqlite')
+
+class DevelopmentConfig(Config):
+    FLASK_ENV='development'
+    DEBUG=True
+
+class TestingConfig(Config):
+    ENV='testing'
+    DATABASE=os.path.join(flaskapp.app.instance_path, 'test.sqlite')
